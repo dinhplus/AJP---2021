7a12459a262a334a69d4a74d60bb10ac970f99be
Dinh Plus <52914286+dinhplus@users.noreply.github.com>
2021-09-23 09:32:43 +0700

    Upload first code

--- src/Thi/Bai2/MultipleThreadCustomize.java
+++ src/Thi/Bai2/MultipleThreadCustomize.java
@@ -4,33 +4,43 @@ public class MultipleThreadCustomize {
 	int counter = 0;
 
     static int N;
+    int i = 0;
+    int j = 0;
 	public MultipleThreadCustomize() {
 	}
 
 	public MultipleThreadCustomize(int N) {
 		this.N = N;
 	}
-    public void printOddNumber()
-    {
-        synchronized (this)
-        {
-            while (counter < N) {
-                while (counter % 2 == 0) {
-                    try {
-                        wait();
-                    }
-                    catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                System.out.println("--" + counter);
-                counter += 1;
-                notify();
-            }
-        }
-    }
-    
+	
+	    public void printOddNumber()
+	    {
+	        synchronized (this)
+	        {
+	            while (counter < N) {
+	                while (counter % 2 == 0) {
+	                    try {
+	                        wait();
+	                    }
+	                    catch (InterruptedException e) {
+	                        e.printStackTrace();
+	                    }
+	                }
+	                if(counter == 1) {
+	                    System.out.println("--" + counter);
+	                    System.out.println("--" + (counter+2));
+	                    i = counter + 4;
+	                } else {
+	                    System.out.println("--" + i);
+	                    System.out.println("--" + (i + 2));
+	                    i+=4;
+	                }
+	                counter++;
+	                notify();
+	            }
+	        }
+	    }
+  
     public void printEvenNumber()
     {
         synchronized (this)
